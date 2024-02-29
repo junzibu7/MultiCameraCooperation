43e14498faa28013b7b4041765c3f951add5555a
hezijia <hzj021010@sjtu.edu.cn>
2024-02-29 19:03:32 +0800

    create a new depository for cooperative observation with multiple cameras

--- src/multi_camera_fusion_ros_test.cpp
+++ src/multi_camera_fusion_ros_test.cpp
@@ -1,19 +1,19 @@
 //
 // Created by hzj on 24-2-28.
 //
-#include <multi_camera_fusion/multi_camera_fusion_ros_test.h>
+#include <multi_camera_cooperation/multi_camera_cooperation_ros_test.h>
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
-    ROS_INFO("Initializing MultiCameraFusion ...");
-    ros::init(argc, argv, "multi_camera_fusion_ros");
+    ROS_INFO("Initializing MultiCameraCooperation ...");
+    ros::init(argc, argv, "multi_camera_cooperation_ros");
     ros::NodeHandle nh("~");
 //    ros::NodeHandle nh;
     ros::Rate rate(30);
 
-    std::cout << "Init MultiCameraFusion" << std::endl;
+    std::cout << "Init MultiCameraCooperation" << std::endl;
 
     ros::spin();
 }
